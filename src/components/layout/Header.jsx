6fec9468ec75294e591317dd63ac0cec5d733d55
Ali <plewffy@gmail.com>
2023-08-29 19:04:27 +0600

    add: header

---
+++ src/components/layout/Header.jsx
@@ -0,0 +1,44 @@
+import { Link, NavLink } from "react-router-dom"
+import logo from '/dive-sea.svg'
+import search from '../../assets/icons/search.svg'
+
+const links = [
+  'Discover',
+  'creators',
+  'Sell',
+  'stats'
+]
+
+function Header() {
+  return (
+    <header className="py-[3.33vw]">
+      <div className="max-w-container mx-auto flex justify-between">
+        <nav className="flex justify-between items-center w-full">
+          <Link to='/dive-sea/'>
+            <img className="w-[3.68vw]" src={logo} alt="DiveSea" />
+          </Link>
+          <ul className="flex gap-[3.71vw]">
+            {
+              links.map(link => {
+                return (
+                  <li className="font-inter text-grey-200 font-medium" key={link}>
+                    <NavLink style={({ isActive }) => isActive ? {color: '#141416'} : null} className="uppercase text-navLink" to={link.toLowerCase()}>{link}</NavLink>
+                  </li>
+                )
+              })
+            }
+          </ul>
+          <div className="flex items-center gap-[2vw]">
+            <div className="flex gap-[1.78vw] bg-grey-0 py-18 pr-[5.66vw] pl-[1.56vw] rounded-[1vw]">
+              <img className="w-[1.5vw]" src={search} alt="" />
+              <input className="text-[1.1vw] text-black-100 placeholder:text-grey-100" type="text" placeholder="Search Art Work / Creator" />
+            </div>
+            <Link to='/dive-sea/sell' className="uppercase font-inter font-semibold text-[1.12vw] bg-black-100 text-white py-[1.12vw] px-[1.68vw] rounded-[1.12vw]">Connect Wallet</Link>
+          </div>
+        </nav>
+      </div>
+    </header>
+  )
+}
+
+export default Header
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,39 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: [],
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    './index.html',
+  ],
   theme: {
-    extend: {},
-  },
-  plugins: [],
+    fontFamily: {
+      'poppins': ['Poppins', 'sans-serif'],
+      'inter': ['Inter', 'sans-serif']
+    },
+    fontSize: {
+      navLink: '1.25vw'
+    },
+    padding: {
+      18: '1.25vw'
+    },
+    borderRadius: {
+      DEFAULT: '1.1vw',
+    },
+    colors: {
+      black: {
+        0: '#000000',
+        100: '#141416',
+      },
+      white: '#ffffff',
+      'grey': {
+        0: '#EDEDED',
+        100: '#C2C3CB',
+        200: '#606060',
+        300: '#F9F9F9',
+      },
+    },
+    maxWidth: {
+      container: '86%'
+    }
+  }
+  
 }
-
